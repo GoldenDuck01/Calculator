f195b8c38670cf6819d5550cb89997b3c6e24d91
Felix Kunze <26.felix.ku@web.de>
2026-04-07 22:21:48 +0200

    Made the absolute Basic for the Calculator

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Made by GoldenDuck, startet on 07.04.2026
 using System;
+using System.Collections;
 
 namespace Calculator
 {
@@ -14,13 +15,43 @@ namespace Calculator
 
         public void Start()
         {
-            System.Console.WriteLine("Bitte geben sie den Term ein, für Hilfe, wie der Taschenrechner zu nutzen ist geben sie help ein");
+            Console.Clear();
+
+            System.Console.WriteLine("Type in your equasion under here!");
+            System.Console.WriteLine();
+            System.Console.WriteLine("If you need help using this Calculator write help instead of your equasion!");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Type exit if you want to end the Calculator!");
 
             string? input = Console.ReadLine();
 
-            if (input == "help")
+            switch (input)
             {
-                System.Console.WriteLine("Es ");
+                case "help":
+                    Console.Clear();
+                    System.Console.WriteLine("Seems like you need some Help ;)");
+                    System.Console.WriteLine("Its easy, just write your equasion almost like you would normaly!");
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("Type in a Number like 3 or any other number and then type in a Operator like + or - for example");
+                    System.Console.WriteLine("Heres every possible Operator:");
+                    System.Console.WriteLine("+ for Addition");
+                    System.Console.WriteLine("- for Subtraction");
+                    System.Console.WriteLine("* for Multiplication");
+                    System.Console.WriteLine("/ for Division");
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("Thats it! Hope you now know how to use this Calculator!");
+                    System.Console.WriteLine("Press any Key to return to the Beginning");
+                    Console.ReadKey();
+                    Start();
+                    break;
+
+                case "exit":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    System.Console.WriteLine("Seems like you misstyped something there, try again!");
+                    break;
             }
         }
 
