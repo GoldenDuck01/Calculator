2a4dcccfc67d49be5a8473b7699e0b93bae40bab
GoldenDuck <26.felix.ku@web.de>
2026-04-07 22:07:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Made by GoldenDuck, startet on 07.04.2026
+using System;
+
+namespace Calculator
+{
+    public class Programm()
+    {
+        public static void Main()
+        {
+            Programm main = new();
+            main.Start();
+        }
+
+        public void Start()
+        {
+            System.Console.WriteLine("Bitte geben sie den Term ein, für Hilfe, wie der Taschenrechner zu nutzen ist geben sie help ein");
+
+            string? input = Console.ReadLine();
+
+            if (input == "help")
+            {
+                System.Console.WriteLine("Es ");
+            }
+        }
+
+    }
+}
